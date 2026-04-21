49e691e5d9d1ae045814112a7e21aedf9a13ccea
Timon Reis <timonreis@gmx.at>
2026-04-21 13:01:08 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,10 +6,10 @@
 /*   By: timonreis <tireis@student.42vienna.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 17:46:55 by timonreis         #+#    #+#             */
-/*   Updated: 2026/04/21 12:32:30 by timonreis        ###   ########.fr       */
+/*   Updated: 2026/04/21 13:00:43 by timonreis        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+#include <stddef.h>
 
 char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -17,22 +17,18 @@ char	*strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
+	if (!needle[0])
+		return ((char *)haystack);
 	while (i < len && haystack[i])
 	{
-		if (haystack[i] == needle[j])
-			return (*needle);
-		if (*needle == '\0')
-			return (NULL);
-		if (haystack[i] != needle[j])
-			return (*haystack);
+		j = 0;
+		while (needle[j] && (i + j) < len && haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+		if (!needle[j])
+			return ((char *)&haystack[i]);
 		i++;
 	}
-}
-
-int	main(void)
-{
-	char	*a;
-
-	a = strnstr("Timon", "im", 5);
-	printf("%s", a);
+	return (NULL);
 }
