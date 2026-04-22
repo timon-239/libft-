e7d58db70147232259d35d06e722e3a9be7516e8
Timon Reis <timonreis@gmx.at>
2026-04-22 10:39:55 +0200

    Fehler bei strdup gefixed

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,27 +6,21 @@
 /*   By: timonreis <tireis@student.42vienna.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 19:01:00 by timonreis         #+#    #+#             */
-/*   Updated: 2026/04/22 10:14:15 by timonreis        ###   ########.fr       */
+/*   Updated: 2026/04/22 10:39:41 by timonreis        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+
 char	*ft_strdup(const char *s1)
 {
 	char	*p1;
-	int		len;
+	size_t	len;
 
 	len = ft_strlen(s1);
-	p1 = malloc(len);
+	p1 = malloc(sizeof(char) * (len + 1));
 	if (!p1)
 		return (NULL);
+	ft_memcpy(p1, s1, len);
+	p1[len] = '\0';
 	return (p1);
-}
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*s;
-
-	s = ft_strdup("HALLO");
-	printf("%s", s);
 }
