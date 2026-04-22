fd300b1270a7be8fa607d14681412a7471d03d67
Timon Reis <timonreis@gmx.at>
2026-04-22 17:30:50 +0200

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,12 @@
 /*   By: tireis <tireis@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 15:12:16 by tireis            #+#    #+#             */
-/*   Updated: 2026/04/22 15:58:41 by tireis           ###   ########.fr       */
+/*   Updated: 2026/04/22 17:30:37 by tireis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static size_t	ft_countword(char const *s, char c)
+static size_t	ft_countwords(char const *s, char c)
 {
 	size_t	count;
 
@@ -36,7 +36,33 @@ char	**ft_split(char const *s, char c)
 	size_t	word_len;
 
 	i = 0;
-	res = malloc(sizeof(char *) * ft_counwords(s, c) + 1);
-	if (!res || !lst)
+	res = malloc(sizeof(char *) * ft_countwords(s, c) + 1);
+	if (!res || !s)
 		return (NULL);
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			if (!ft_strchr(s, c))
+				word_len = ft_strlen(s);
+			else
+				word_len = ft_strchr(s, c) - s;
+			res[i++] = ft_substr(s, 0, word_len);
+			s += word_len;
+		}
+	}
+	return (res);
 }
+/*#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+		return (0);
+	else
+		for(int i = 0; i < ft_strlen(av[]))
+		{
+
+		}
+}*/
