34a9f3689b7d4557a2e38e4b3a53bfa4625b8c2e
Timon Reis <timonreis@gmx.at>
2026-04-27 12:34:41 +0200

    changes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tireis <tireis@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 14:12:42 by tireis            #+#    #+#             */
-/*   Updated: 2026/04/26 21:58:09 by tireis           ###   ########.fr       */
+/*   Updated: 2026/04/27 12:34:33 by tireis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,17 @@ void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
 void ft_putendl_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
+t_list *ft_lstnew(void *content);
+void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
+void ft_lstdelone(t_list *lst, void (*del)(void
+*));
+void ft_lstclear(t_list **lst, void (*del)(void
+*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *),
+void (*del)(void *));
 
 #endif
