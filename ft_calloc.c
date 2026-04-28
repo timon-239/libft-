5c345de94374ed13de067490d155bdb7af587455
Timon Reis <timonreis@gmx.at>
2026-04-28 12:15:29 +0200

    calloc bug

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tireis <tireis@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 14:06:29 by tireis            #+#    #+#             */
-/*   Updated: 2026/04/22 14:06:38 by tireis           ###   ########.fr       */
+/*   Updated: 2026/04/28 12:15:19 by tireis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	totalsize;
 	char	*ptr;
 
-	if (!count && size > SIZE_MAX / count)
+	if (count != 0 && size > ((size_t)-1) / count)
 		return (NULL);
 	totalsize = count * size;
 	ptr = malloc(totalsize);
